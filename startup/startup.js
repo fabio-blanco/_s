3f7d0c7675acf3851b07e7935b61b937afc8f5a4
Fabio Blanco <fabio@fbsoftware.com.br>
2021-12-27 19:17:59 -0300

    Added to startup script an option to strip off git files from the theme folder and made some minor bug fixes

--- startup/startup.js
+++ startup/startup.js
@@ -6,27 +6,21 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question("Type the name of the theme: ", function(themeName) {
-    shell.echo("The theme name '" + themeName + "' will be used!");
-
-    let errorCode = validateThemeName(themeName);
-
-    if (!errorCode) {
-        errorCode = replaceThemeName(themeName);
-    }
-
-    rl.close();
-
-    if (errorCode) {
-        return errorCode;
-    }
-});
-
 function validateThemeName(themeName) {
     if (/\s/g.test(themeName)) {
         shell.echo("Invalid theme name. Please don't use spaces!");
         return 1;
     }
+}
+
+function doStartUp(themeName, removeGitFolders) {
+    let errorCode = replaceThemeName(themeName);
+
+    if (!errorCode && removeGitFolders) {
+        errorCode = doRemoveGitFolders();
+    }
+
+    return errorCode;
 }
 
 function replaceThemeName(themeName) {
@@ -58,10 +52,11 @@ function replaceThemeName(themeName) {
     shell.ls('*.php', 'inc/*.php', 'template-parts/*.php').forEach(function(file) {
         //4. Search for <code>&nbsp;_s</code> (with a space before it) to capture DocBlocks and replace with: <code>&nbsp;Megatherium_is_Awesome</code>.
         shell.sed('-i', / esqueleto/g, ' ' + themeName, file);
+        shell.sed('-i', /\sesqueleto/g, '\s' + themeName, file);
         //5. Search for `_s-` to capture prefixed handles and replace with: `megatherium-is-awesome-`.
         shell.sed('-i', /esqueleto-/g, themeName + "-", file);
         //6. Search for `_S_` (in uppercase) to capture constants and replace with: `MEGATHERIUM_IS_AWESOME_`.
-            shell.sed('-i', /ESQUELETO_/g, themeName.toUpperCase() + "_", file);
+        shell.sed('-i', /ESQUELETO_/g, themeName.toUpperCase() + "_", file);
     });
 
     shell.sed('-i', /"esqueleto"/g, '"' + themeName + '"', 'phpcs.xml.dist');
@@ -69,3 +64,46 @@ function replaceThemeName(themeName) {
 
     shell.echo("The theme startup is ready!");
 }
+
+
+function doRemoveGitFolders() {
+    const {code} = shell.rm('-rf', ['.git', '.github', '.gitignore', '.gitattributes']);
+
+    return code;
+}
+
+function main() {
+    rl.question("Type the name of the theme: ", function(themeName) {
+        shell.echo("The theme name '" + themeName + "' will be used!");
+
+        let errorCode = validateThemeName(themeName);
+
+        if (!errorCode) {
+
+            rl.question("Strip off git files? (Y, n): ", function (answer) {
+                let removeGitFolders = true;
+
+                if (answer && answer.trim().toLowerCase() === 'n') {
+                    removeGitFolders = false;
+                } else if(answer && answer.trim().toLowerCase() !== 'y') {
+                    shell.echo("Unknown option! Please provide one of the following: y, n");
+                    errorCode = 1;
+                }
+
+                if (!errorCode) {
+                    errorCode = doStartUp(themeName, removeGitFolders);
+                }
+
+                rl.close();
+
+            });
+
+        }
+
+        if (errorCode) {
+            return errorCode;
+        }
+    });
+}
+
+main();
